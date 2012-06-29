10010521d70710f8b907b0f2a5c52eae7ad2a132
Emilian Peev <epeev@mm-sol.com>
2012-06-29 14:04:21 +0300

    CameraHal: Adds support for OMAP5 video HQ pipe
    - The new capture mode is added through a compile
      time flag for OMAP5 exclusively.
    
    Change-Id: Ida43b611783ce15a2c34371ef4cad98bfa03f379
    Signed-off-by: Emilian Peev <epeev@mm-sol.com>
    Signed-off-by: Daniel Levin <dendy@ti.com>

--- camera/OMXCameraAdapter/OMXCameraAdapter.cpp
+++ camera/OMXCameraAdapter/OMXCameraAdapter.cpp
@@ -827,7 +827,8 @@ void OMXCameraAdapter::getParameters(android::CameraParameters& params)
            params.set(android::CameraParameters::KEY_FLASH_MODE, valstr);
 
        if ((mParameters3A.Focus == OMX_IMAGE_FocusControlAuto) &&
-           (mCapMode != OMXCameraAdapter::VIDEO_MODE)) {
+           ( (mCapMode != OMXCameraAdapter::VIDEO_MODE) &&
+             (mCapMode != OMXCameraAdapter::VIDEO_MODE_HQ) ) ) {
            valstr = android::CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE;
        } else {
            valstr = getLUTvalue_OMXtoHAL(mParameters3A.Focus, FocusLUT);
@@ -1993,7 +1994,8 @@ status_t OMXCameraAdapter::UseBuffersPreview(CameraBuffer * bufArr, int num)
             }
         }
 
-        if(mCapMode == OMXCameraAdapter::VIDEO_MODE) {
+        if( (mCapMode == OMXCameraAdapter::VIDEO_MODE) ||
+            (mCapMode == OMXCameraAdapter::VIDEO_MODE_HQ) ) {
 
             if (mPendingPreviewSettings & SetVNF) {
                 mPendingPreviewSettings &= ~SetVNF;
@@ -2841,7 +2843,8 @@ status_t OMXCameraAdapter::getFrameSize(size_t &width, size_t &height)
             }
         }
 
-        if(mCapMode == OMXCameraAdapter::VIDEO_MODE) {
+        if((mCapMode == OMXCameraAdapter::VIDEO_MODE) ||
+           (mCapMode == OMXCameraAdapter::VIDEO_MODE_HQ) ) {
 
             if (mPendingPreviewSettings & SetVNF) {
                 mPendingPreviewSettings &= ~SetVNF;
@@ -3502,7 +3505,7 @@ OMX_ERRORTYPE OMXCameraAdapter::OMXCameraAdapterFillBufferDone(OMX_IN OMX_HANDLE
         //            if we are waiting for a snapshot and in video mode...go ahead and send
         //            this frame as a snapshot
         if( mWaitingForSnapshot &&  (mCapturedFrames > 0) &&
-            (snapshotFrame || (mCapMode == VIDEO_MODE)))
+            (snapshotFrame || (mCapMode == VIDEO_MODE) || (mCapMode == VIDEO_MODE_HQ ) ))
             {
             typeOfFrame = CameraFrame::SNAPSHOT_FRAME;
             mask = (unsigned int)CameraFrame::SNAPSHOT_FRAME;
@@ -3532,13 +3535,15 @@ OMX_ERRORTYPE OMXCameraAdapter::OMXCameraAdapterFillBufferDone(OMX_IN OMX_HANDLE
         //CAMHAL_LOGV("FBD pBuffer = 0x%x", pBuffHeader->pBuffer);
 
         if( mWaitingForSnapshot )
-          {
-            if (!mBracketingEnabled &&
-                 ((HIGH_SPEED == mCapMode) || (VIDEO_MODE == mCapMode)) )
-              {
-                notifyShutterSubscribers();
-              }
-          }
+            {
+            if ( !mBracketingEnabled &&
+                 ((HIGH_SPEED == mCapMode) ||
+                  (VIDEO_MODE == mCapMode) ||
+                  (VIDEO_MODE_HQ == mCapMode)) )
+                {
+                    notifyShutterSubscribers();
+                }
+            }
 
         stat = sendCallBacks(cameraFrame, pBuffHeader, mask, pPortParam);
         mFramesWithDisplay++;
@@ -4533,6 +4538,19 @@ public:
             if ( NO_ERROR != err ) {
                 return err;
             }
+
+#ifdef CAMERAHAL_OMAP5_CAPTURE_MODES
+
+            CAMHAL_LOGD("Camera mode: VIDEO HQ ");
+            properties->setMode(MODE_VIDEO_HIGH_QUALITY);
+            err = fetchCapabiltiesForMode(OMX_CaptureHighQualityVideo,
+                                          sensorId,
+                                          properties);
+            if ( NO_ERROR != err ) {
+                return err;
+            }
+
+#endif
 
         }
 #endif
