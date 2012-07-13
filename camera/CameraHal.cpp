98ff515db818400410d1cd03bf90c123b88726be
Emilian Peev <epeev@mm-sol.com>
2012-07-13 12:36:52 +0300

    CameraHal: Fix instability with camera close
    - 'mBufferSourceAdapter_Out' and 'mBufferSourceAdapter_In'
      were not being cleared and as a result the reference count
      to their associated BufferSoruceAdapters was kept above
      zero throughout CameraHals destructor. The count goes to
      zero upon leaving the destructor, which triggers their
      destruction but at this point the CameraAdapter reference
      is invalid ( the CameraAdapter got destroyed before that)
      and every access or call to it will result in a segfault.
    
    Change-Id: I23b2c59789eb47ba02884a46982ae10a18e4916b
    Signed-off-by: Emilian Peev <epeev@mm-sol.com>
    Signed-off-by: Vladimir Petrov <vppetrov@mm-sol.com>

--- camera/CameraHal.cpp
+++ camera/CameraHal.cpp
@@ -4251,6 +4251,9 @@ void CameraHal::deinitialize()
         mSensorListener = NULL;
     }
 
+    mBufferSourceAdapter_Out.clear();
+    mBufferSourceAdapter_In.clear();
+
     LOG_FUNCTION_NAME_EXIT;
 
 }
